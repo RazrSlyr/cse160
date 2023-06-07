bad08c6d2c34f699170cab853efd64fb8ee223e0
Rohan Venkatapuram <sharpedo75@gmail.com>
2023-06-07 11:06:37 -0700

    Added back grass texture

---
+++ asgn5/src/World/components/materials.js
@@ -0,0 +1,17 @@
+import { MeshPhongMaterial, RepeatWrapping, TextureLoader } from "../../../lib/three.module";
+
+function getGrassMaterial() {
+    let texturePath = "../../../assets/textures/grass/";
+    let textureParts = ["aoMap", "bumpMap", "map", "normalMap", "roughnessMap"];
+    const textureLoader = new TextureLoader();
+    const mat = new MeshPhongMaterial();
+    for (const part of textureParts) {
+        textureLoader.loadAsync(`${texturePath}${textureParts}.png`).then((texture) => {
+            texture.wrapS = texture.wrapT = RepeatWrapping;
+            texture.repeat.set(20, 20);
+            mat[part] = texture;
+            mat.needsUpdate = true;
+        })
+    }
+    return mat;
+}
